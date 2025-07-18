106dff548b3ea3e41107ccff1fe2dad601b28a25
lucasdaniel2201 <lucasdsantos2201@gmail.com>
2025-07-18 18:46:21 -0300

    leve correção no database

--- config/database.js
+++ config/database.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize({
   dialect: 'sqlite',
+  storage: './database.sqlite',
   logging: false,
-  storage: './database.sqlite'
 });
 
 module.exports = sequelize;
